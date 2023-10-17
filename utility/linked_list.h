d862221cc2c1fe8a95e43d7048cf8c87d661cbf9
jmtinycircuits <jmarcum@tinycircuits.com>
2023-10-17 11:56:43 -0400

    Handle linked list node deletion bettwer and remove is_end_node flag

--- utility/linked_list.h
+++ utility/linked_list.h
@@ -11,7 +11,6 @@ typedef struct linked_list_node{
     void *object;
     struct linked_list_node *next;
     struct linked_list_node *previous;
-    bool is_end_node;
 } linked_list_node;
 
 
@@ -26,9 +25,12 @@ typedef struct{
 linked_list_node *linked_list_add_obj(linked_list *list, void *obj){
     ENGINE_INFO_PRINTF("Object List: adding object");
 
-    // Allocate a new node
+    // Allocate a new node, set defaults
     linked_list_node *new_node = list->end->next;
     new_node = malloc(sizeof(linked_list_node));
+    new_node->next = NULL;
+    new_node->previous = NULL;
+    new_node->object = NULL;
 
     // Set the start node to the new node if not initialzed and point the end to it
     if(list->initialzed == false){
@@ -41,11 +43,9 @@ linked_list_node *linked_list_add_obj(linked_list *list, void *obj){
     // Put data in the new node and link to previous 
     new_node->object = obj;
     new_node->previous = list->end;
-    new_node->is_end_node = true;
 
     // Add new node to list and set the end of the list to it
     list->end->next = new_node;
-    list->end->is_end_node = false;
     list->end = list->end->next;
 
     // Return pointer to the new node so it can be removed easily
@@ -57,18 +57,22 @@ linked_list_node *linked_list_add_obj(linked_list *list, void *obj){
 void linked_list_del_list_node(linked_list *list, linked_list_node *node){
     ENGINE_INFO_PRINTF("Object List: removing object");
 
-    // Relink nodes around the node being deleted
-    if(node->previous != NULL) node->previous->next = node->next;
-    if(node->next != NULL) node->next->previous = node->previous;
-
-    // If the node being deleted is the end node,
-    // set the previous node as the end node
-    if(node->is_end_node && node->previous != NULL){
-        node->previous->is_end_node = true;
+    // Only the 'start' node can have a NULL previous node, relink 
+    // 'start' to its next if that's also not null. Otherwise, if
+    // 'previous' and 'next' are NULL set list to uninitialized
+    if(node->previous == NULL && node->next == NULL){   // Only one node in list, the 'start' node
+        list->initialzed = false;
+    }else if(node->previous == NULL){                   // Deleting 'start' but there are other nodes, link start to next
+        list->start = node->next;
+        list->start->previous = NULL;
+    }else if(node->next == NULL){                       // Deleting 'end' but there are other nodes, link end to previous
         list->end = node->previous;
+        list->end->next = NULL;
+    }else{                                              // Deleting a node in middle, relink around
+        node->previous->next = node->next;
+        node->next->previous = node->previous;
     }
 
-    // Free the memory taken up by the now unreferenced node
     free(node);
 }
 
