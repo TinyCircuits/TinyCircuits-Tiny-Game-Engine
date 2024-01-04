68e8604872effce3930cb39b1cacd432e0021ec6
jmtinycircuits <jmarcum@tinycircuits.com>
2024-01-04 12:49:01 -0500

    Fix buffer switching order issue on rp2 port

--- display/engine_display_driver_rp2_st7789.c
+++ display/engine_display_driver_rp2_st7789.c
@@ -194,16 +194,16 @@ void engine_display_st7789_init(){
 
 
 void engine_display_st7789_update(uint16_t *screen_buffer_to_render){
-    // Point DMA to active screen buffer that should be
-    // sent once the last frame is finished sending
-    txbuf = screen_buffer_to_render;
-
     if(dma_channel_is_busy(dma_tx)){
         ENGINE_WARNING_PRINTF("Waiting on previous DMA transfer to complete. Could have done more last frame!");
-        // ENGINE_PERFORMANCE_START(ENGINE_PERF_TIMER_2);
+        ENGINE_PERFORMANCE_START(ENGINE_PERF_TIMER_2);
         dma_channel_wait_for_finish_blocking(dma_tx);
-        // ENGINE_PERFORMANCE_STOP(ENGINE_PERF_TIMER_2, "Time spent waiting on remaining DMA");
+        ENGINE_PERFORMANCE_STOP(ENGINE_PERF_TIMER_2, "Time spent waiting on remaining DMA");
     }
+
+    // Point DMA to active screen buffer that should be
+    // sent now that the last frame is finished sending
+    txbuf = screen_buffer_to_render;
 
     st7789_reset_window();
 
