8005d3045d6a5dc1d0502d094e4668009104d59f
TPReal <tpreal@gmail.com>
2024-07-17 17:09:19 +0200

    Freeze test.

---
+++ frozen_manifest.py
@@ -0,0 +1,7 @@
+# Pass to make as:
+# FROZEN_MANIFEST=../../TinyCircuits-Tiny-Game-Engine/frozen_manifest.py
+
+# Uncomment if there is a board manifest that should be included.
+# include("$(BOARD_DIR)/manifest.py")
+
+freeze("frozen")
