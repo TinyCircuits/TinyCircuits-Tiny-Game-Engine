1dfcdb2ed53dc89ad7d5b7b2ca73be26d070aad3
TinyCircuits <info@tinycircuits.com>
2024-07-24 16:36:26 -0400

    Add a way to see if RTC has been compromised in RTCTest example

--- filesystem/Games/RTCTest/main.py
+++ filesystem/Games/RTCTest/main.py
@@ -12,3 +12,6 @@ while True:
         if engine_io.B.is_just_pressed:
             print("Set")
             engine_time.datetime((2024, 7, 24, 11, 49, 0))
+        if engine_io.UP.is_just_pressed:
+            print("Is compromised?")
+            print(engine_time.is_compromised())
