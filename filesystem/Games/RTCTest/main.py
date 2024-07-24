935dbacd71fb6b2967a587486e0c4797c889db90
TinyCircuits <info@tinycircuits.com>
2024-07-24 16:05:08 -0400

    Finish RTC for hardware (UNIX port still remains)

---
+++ filesystem/Games/RTCTest/main.py
@@ -0,0 +1,14 @@
+import engine_main
+import engine
+import engine_time
+import engine_io
+
+
+while True:
+    if engine.tick():
+        if engine_io.A.is_just_pressed:
+            print("Set")
+            print(engine_time.datetime())
+        if engine_io.B.is_just_pressed:
+            print("Get")
+            engine_time.datetime((2024, 7, 24, 11, 49, 0))
---
+++ src/time/engine_rtc.h
@@ -0,0 +1,25 @@
+#ifndef ENGINE_RTC_H
+#define ENGINE_RTC_H
+
+#include <time.h>   // For time struct that bm8563 library uses
+#include <stdint.h>
+#include <stdbool.h>
+
+#define ENGINE_RTC_OK 1
+#define ENGINE_RTC_I2C_ERROR -1
+
+// Sets up I2C pins for RTC clock and checks if
+// time has been compromised (i.e. lost power)
+void engine_rtc_init();
+
+// Checks if the RTC may have gone below voltage and lost time.
+// Returns true of compromised
+bool engine_rtc_check_compromised();
+
+// Get the broken down time
+int engine_rtc_get_datetime(struct tm *rtc);
+
+// Set the broken down time
+int engine_rtc_set_datetime(struct tm *rtc);
+
+#endif  // ENGINE_RTC_H
