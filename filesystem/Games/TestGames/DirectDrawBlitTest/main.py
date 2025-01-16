7c786fa526bb63603c002f3585048c21c1ac28cf
TinyCircuits <info@tinycircuits.com>
2025-01-16 14:51:25 -0500

    Update direct blit example

--- filesystem/Games/TestGames/DirectDrawBlitTest/main.py
+++ filesystem/Games/TestGames/DirectDrawBlitTest/main.py
@@ -8,8 +8,8 @@ from engine_resources import TextureResource
 texture = TextureResource("Games/TestGames/DirectDrawBlitTest/32x32.bmp")
 
 while True:
-    engine_draw.clear(0)
-    engine_draw.blit(texture, 32, 0,  0.75)
-    engine_draw.blit(texture, 32, 32, 0.25)
-    engine_draw.blit(texture, 32, 96, 1.0)
+    engine_draw.clear(0xffff)
+    engine_draw.blit(texture, 32, 0,  engine_draw.black, 0.75)
+    engine_draw.blit(texture, 32, 32, engine_draw.black, 0.25)
+    engine_draw.blit(texture, 32, 96, engine_draw.black, 1.0)
     engine_draw.update()
